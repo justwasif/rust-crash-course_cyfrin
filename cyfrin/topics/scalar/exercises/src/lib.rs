3cb4069eaa42438d1a15e5c8f251f9ccd041aed2
wasif <wasif.iit.ansari@gmail.com>
2026-03-07 14:40:52 +0530

    feat:scalar exercise

--- cyfrin/topics/scalar/exercises/src/lib.rs
+++ cyfrin/topics/scalar/exercises/src/lib.rs
@@ -1,11 +1,18 @@
-pub fn eq() {
-    todo!();
+
+pub fn eq(x:char,y:char) ->bool{
+    if x==y{
+        return true;
+    }else {
+        return false;
+    }
 }
 
-pub fn add() {
-    todo!();
+pub fn add(x:f32,y:f32,z:f32)->f32 {
+    let sum:f32=x+y+z;
+    return sum;
 }
 
 pub fn cast(x: u8, y: i8, z: f32) -> f32 {
-    x + y + z
+    let sum=x as f32 + y as f32 + z; 
+    return sum;
 }
