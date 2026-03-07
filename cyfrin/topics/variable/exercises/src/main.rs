ab6e9d51c17337f5853fa2078a9b7b102d27ecf6
wasif <wasif.iit.ansari@gmail.com>
2026-03-07 06:40:40 +0530

    feat:variable exercise

--- cyfrin/topics/variable/exercises/src/main.rs
+++ cyfrin/topics/variable/exercises/src/main.rs
@@ -3,7 +3,12 @@
 
 fn main() {
     // Exercise 1: Make this variable mutable
-    let count = 1;
+    let mut count = 1;
+    // println!("x:{}",count);
+    // println!("x:{}",count);
+    // println!("{0}+{0}={1}",count,count+count);
+    // println!("x:{}",count);
+    // println!("x:{}",count);
     count += 1;
 
     println!("count: {count}");
