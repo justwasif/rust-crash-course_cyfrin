aff349d4dcf1e6056d869a84cacde19ec795e891
wasif <wasif.iit.ansari@gmail.com>
2026-03-07 08:15:56 +0530

    feat:function exercise

--- cyfrin/topics/function/exercises/src/lib.rs
+++ cyfrin/topics/function/exercises/src/lib.rs
@@ -1,7 +1,10 @@
-pub fn mul() {
-    todo!();
+pub fn mul(x:u32,y:u32)->u32 {
+    let z=x*y;
+    return z;
 }
 
-pub fn div() {
-    todo!();
+pub fn div(x:u32,y:u32)->u32 {
+    let z=x/y;
+    return z;
 }
+
