c6ed255ad60d5e89ccada8fa3c4014bbd21f3a8a
wasif <wasif.iit.ansari@gmail.com>
2026-03-08 07:24:51 +0530

    feat:tuple exercise

--- cyfrin/topics/tuple/exercises/src/lib.rs
+++ cyfrin/topics/tuple/exercises/src/lib.rs
@@ -1,11 +1,14 @@
 pub fn first(t: (bool, u32, char)) -> bool {
-    todo!();
+    return t.0;
+   
 }
 
 pub fn last(t: (bool, u32, char)) -> char {
-    todo!();
+    return t.2;
 }
 
 pub fn swap(t: (u32, u32)) -> (u32, u32) {
-    todo!();
+    let (a,b)=t;
+    let (b,a)=(a,b);
+    return (a,b);
 }
