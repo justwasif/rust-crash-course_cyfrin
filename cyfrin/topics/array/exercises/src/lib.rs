3838ddc17ecb41ccc460209c89f7670b77c51f1d
wasif <wasif.iit.ansari@gmail.com>
2026-03-08 08:06:02 +0530

    feat:arrays exercise

--- cyfrin/topics/array/exercises/src/lib.rs
+++ cyfrin/topics/array/exercises/src/lib.rs
@@ -1,11 +1,15 @@
 pub fn zeros() -> [u32; 100] {
-    todo!();
+    let arr=[0;100];
+    return arr
 }
 
 pub fn first_3(s: &[u32]) -> &[u32] {
-    todo!();
+    let first3=&s[..3];
+    return first3;
 }
 
 pub fn last_3(s: &[u32]) -> &[u32] {
-    todo!();
+    let n=s.len();
+    let last3=&s[n-3..];
+    return last3;
 }
