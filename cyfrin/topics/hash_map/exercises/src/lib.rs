0ba6ae20664e38ff1fd2e59288fd1dde9afaacee
wasif <wasif.iit.ansari@gmail.com>
2026-03-11 09:06:26 +0530

    feat:hashmap struck vector ecxercise

--- cyfrin/topics/hash_map/exercises/src/lib.rs
+++ cyfrin/topics/hash_map/exercises/src/lib.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
 pub fn init(address: String, amount: u32) -> HashMap<String, u32> {
-    todo!();
+    let mut H:HashMap<String,u32>=HashMap::new();
+    H.insert(address,amount);
+    return H;
 }
--- cyfrin/topics/vector/exercises/src/lib.rs
+++ cyfrin/topics/vector/exercises/src/lib.rs
@@ -1,3 +1,7 @@
 pub fn init(x: u32, y: u32, z: u32) -> Vec<u32> {
-    todo!();
+    let mut v:Vec<u32>=Vec::new();
+    v.push(x);
+    v.push(y);
+    v.push(z);
+    return v;
 }
